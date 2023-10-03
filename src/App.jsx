8b448765c121bba905ad8f90e995323d5efce401
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-10-03 18:56:19 +0100

    add the close btn state

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,44 @@ const messages = [
 ];
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isClose, setIsClose] = useState(false);
   const handleNext = () => {
-    if (step !== 3) {
-      setStep(step + 1);
-    }
+    if (step !== 3) setStep(step + 1);
   };
   const handlePrevious = () => {
-    if (step !== 1) {
-      setStep(step - 1);
-    }
+    if (step !== 1) setStep(step - 1);
   };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step === 1 ? "active" : ""}`}>1</div>
-        <div className={`${step === 2 ? "active" : ""}`}>2</div>
-        <div className={`${step === 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          className={`${step !== 1 ? "btn-style" : ""}`}
-          onClick={() => handlePrevious()}
-        >
-          Previous
-        </button>
-        <button
-          className={`${step !== 3 ? "btn-style" : ""}`}
-          onClick={() => handleNext()}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsClose(!isClose)}>
+        &times;
+      </button>
+      {isClose || (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step === 1 ? "active" : ""}`}>1</div>
+            <div className={`${step === 2 ? "active" : ""}`}>2</div>
+            <div className={`${step === 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              className={`${step !== 1 ? "btn-style" : ""}`}
+              onClick={() => handlePrevious()}
+            >
+              Previous
+            </button>
+            <button
+              className={`${step !== 3 ? "btn-style" : ""}`}
+              onClick={() => handleNext()}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
